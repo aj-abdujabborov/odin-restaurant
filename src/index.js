163ee744301be9d8244860af618b694b57537df4
AJ Abdujabborov <60819490+aj-abdujabborov@users.noreply.github.com>
2023-11-11 15:52:47 +0500

    Set up Webpack w/ live reload

---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log("S'all good");
---
+++ webpack.common.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Odin Restaurant',
+            template: './src/index.html',
+            inject: 'head',
+            scriptLoading: 'defer',
+        })
+    ],
+    
+}
