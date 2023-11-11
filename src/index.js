7b310cf28d1a5b93356c33c2b7f94067e34c8257
AJ Abdujabborov <60819490+aj-abdujabborov@users.noreply.github.com>
2023-11-11 18:49:17 +0500

    Add page elements through JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import "./style.css"
+import Image from "./Palace+Grill+Restaurant-100.jpg"
 
-console.log("S'all good");
+const divContent = document.querySelector("div#content");
+const h1 = document.createElement('h1');
+const p = document.createElement('p');
+const img = document.createElement('img');
+
+h1.textContent = 'Nexus Roadhouse';
+p.textContent = "We hope you have a great time at the nexus between amazing flavor and texture. We're located on highway 49.";
+img.src = Image;
+img.alt = "grilled foods";
+
+divContent.append(h1, p, img);
+
+
